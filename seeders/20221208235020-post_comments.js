37ffc5256216d517ac09709fe4444b6dc6805804
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-08 18:55:40 -0500

    Created seed files for comments and reposts

---
+++ seeders/20221208235020-post_comments.js
@@ -0,0 +1,20 @@
+'use strict';
+const Post = require('../models/post');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const posts = await Post.findAll({ raw: true });
+		const comments = [...Array(100)].map(() => ({
+			postId: posts[Math.floor(Math.random() * posts.length)].id,
+			commentId: posts[Math.floor(Math.random() * posts.length)].id,
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}));
+		await queryInterface.bulkInsert('post_comments', comments, {});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('post_comments');
+	}
+};
---
+++ seeders/20221208235025-post_reposts.js
@@ -0,0 +1,20 @@
+'use strict';
+const Post = require('../models/post');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const posts = await Post.findAll({ raw: true });
+		const reposts = [...Array(100)].map(() => ({
+			postId: posts[Math.floor(Math.random() * posts.length)].id,
+			repostId: posts[Math.floor(Math.random() * posts.length)].id,
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}));
+		await queryInterface.bulkInsert('post_reposts', reposts, {});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('post_reposts');
+	}
+};
