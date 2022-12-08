7c6b1b6451a9072c69d49f84a99eb2d24b66140d
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-08 18:49:53 -0500

    Created post reactions seeder

--- seeders/20221208232621-post_reactions.js
+++ seeders/20221208232621-post_reactions.js
@@ -7,7 +7,17 @@ module.exports = {
 	async up(queryInterface, Sequelize) {
 		const users = await User.findAll({ raw: true });
 		const posts = await Post.findAll({ raw: true });
+		const reactions = [...Array(100)].map(() => ({
+			userId: users[Math.floor(Math.random() * users.length)].id,
+			postId: posts[Math.floor(Math.random() * posts.length)].id,
+			reactionId: Math.floor(Math.random() * 2),
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}));
+		await queryInterface.bulkInsert('post_reactions', reactions, {});
 	},
 
-	async down(queryInterface, Sequelize) {}
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('post_reactions');
+	}
 };
