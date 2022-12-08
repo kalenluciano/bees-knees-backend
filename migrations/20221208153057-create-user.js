1255babd1d05ea15750003c41d2d4e1e9b25839b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-08 10:42:16 -0500

    Created initial user model

---
+++ migrations/20221208153057-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('users', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			firstName: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			lastName: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			username: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			email: {
+				type: Sequelize.STRING
+			},
+			passwordDigest: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true,
+				validate: {
+					isEmail: true
+				}
+			},
+			profilePic: {
+				type: Sequelize.TEXT
+			},
+			coverPhoto: {
+				type: Sequelize.TEXT
+			},
+			bio: {
+				type: Sequelize.TEXT
+			},
+			dateOfBirth: {
+				type: Sequelize.DATE
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('users');
+	}
+};
